8b93b2371e683c05af9db69c47faec9f8813c6ad
MatteoFilosa <filosa.1793997@studenti.uniroma1.it>
2023-12-07 19:29:24 +0100

    Small adjustment and trying the first connection to mongo_db!

--- server_flask.py
+++ server_flask.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template
+import configparser
 import os
-
 
 app = Flask(__name__,
             static_url_path='', 
             static_folder='static',
             template_folder='templates')
 
-
-
 @app.route("/")
 def home():
-    return render_template('index.html') #change this do 'design.html' in order to design a new questionnaire.
+    return render_template('index.html') 
 
+config = configparser.ConfigParser()
+config.read(os.path.abspath(os.path.join(".ini")))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    app.config['DEBUG'] = True
+    app.config['MONGO_URI'] = config['PROD']['DB_URI']
+
     app.run(debug=True)
